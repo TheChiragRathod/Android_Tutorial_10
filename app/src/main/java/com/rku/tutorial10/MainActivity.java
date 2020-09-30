1d88ba678e52386bf6a82204f6bbb8b5e3c0558d
TheChiragRathod <crathod116@rku.ac.in>
2020-09-30 21:26:26 +0530

    MyAsyncTask Class Created in MainActivity.java file.

--- app/src/main/java/com/rku/tutorial10/MainActivity.java
+++ app/src/main/java/com/rku/tutorial10/MainActivity.java
@@ -2,13 +2,35 @@ package com.rku.tutorial10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ListView listView;
+    JSONArray itemjsonArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listView);
+
     }
+
+    //AsyncTask class created to manipulate the process in background --------
+    class MyAsyncTask extends AsyncTask
+    {
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+    }
+
+
+    //AsyncTask class Code finished --------
 }
